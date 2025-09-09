5f0b0002d082cd7235e0fa85b75337a3f4cc49ac
ibrahim020576@gmail.com <ibrahim020576@gmail.com>
2025-09-09 13:42:53 +0300

    операторы сравнения и if else

--- app/script.js
+++ app/script.js
@@ -1,43 +1,50 @@
-//Тестовый комментарий номер 1
-/*
-  Тестовый комментарий номер 2
-*/
-
 'use strict'
 /*
   use strict это строгий режим javascript 
   который не позволяет использовать нестандартные вещи в коде
   и помогает избежать ошибок.
   Без use strict можно не указывать типы переменных, например let, const, var
+  и можно использовать нестандартные символы в переменных, например $, _, @
 */ 
-
-// Выводит в консоль сообщение Hello, world!
-// console.log(
-//   'Hello, world!'
-// );
-
-// Переменная hello со значением Hello, world!
-let hello
-hello = 'Hello, world!'
-// Выводит в консоль переменную hello
-console.log(hello);
-
-// Задаем переменной новое значение
-hello = "Привет, мир!"
-console.log(hello);
 
 /*    
   в перменной let можно задать новое значение и переименовать её
   в перменной const нельзя задать новое значение и переименовать её -
   нужно использовать const, если значение не будет меняться в коде
 */
+let name = 'Владимир';
+const age = 25;
 
-// const myName = "Владимир"
-// const age = 30
 
-// console.log(myName, age + " лет");
+// что бы узнать какой тип данных используется есть функция typeof
+// const isUser = true;
+// console.log(typeof isUser); // выведет boolean
 
-const name = "Владимир" 
-const surname = name + " Кузнецов"
+// Тип данных object нельзя преобразовать в String или Number иначе он выведет в консоль [object Object]
 
-console.log(surname);
+const obj = {
+  name: 'Владимир',
+}
+
+console.log(String(obj)); // Выводит [object Object]
+
+const result = 102 / (21 + +'31') - 23 ** 2 
+
+console.log(result);
+
+// операторы сравнения 
+console.log(10 == 10); // true равно
+console.log(10 != 10); // false не равно
+console.log(10 > 3); // true больше
+console.log(10 < 3); // false меньше
+console.log(10 >= 10); // true больше или равно
+console.log(11 <= 10); // false меньше или равно
+
+let year = 2022;
+const non = false;
+
+if (year == 2023 || non == false) {
+  console.log('Год 2023');
+} else {
+  console.log('Год не 2023');
+}
